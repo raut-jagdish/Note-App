6c186f634567fc9f2950a1be4516521a522f38e7
Jagdish Raut <92374690+raut-jagdish@users.noreply.github.com>
2024-02-17 22:42:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const notesContainer = document.querySelector(".container");
-const createbtn = document.querySelector(".btn");
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-createbtn.addEventListener("click", ()=>{
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", ()=>{
     let inputBox=document.createElement("p");
     let img = document.createElement('img');
-    inputbox.className="input-box"
+    inputBox.className="input-box";
     inputBox.setAttribute("contenteditable", "true");
     img.src="notes-app-img/images/delete.png";
-    notescontainer.appendChild(inputBox).appendChild(img);
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+
+
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName==="P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt=>
+            {
+                nt.onkeyup=function(){
+                    updateStorage(0);
+                }
+            })
+    }
+})
+
+document.addEventListener("keydown",event=>{
+    if(event.key==="Enter"){
+        document.execCommand("ineserLineBreak");
+        event.preventDefault();
+    }
 })
